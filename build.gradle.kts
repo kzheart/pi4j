c71575c96c17b6c420437b249522edeef7ea5daa
kzheart <kzheartgyf@gmail.com>
2026-02-13 11:23:59 +0800

    build(publish): 配置 maven-publish 并更新 1.0.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+
 allprojects {
     group = "com.pi4j"
-    version = "0.1.0-SNAPSHOT"
+    version = "1.0.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
@@ -9,6 +12,7 @@ allprojects {
 
 subprojects {
     apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
 
     extensions.configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -18,6 +22,17 @@ subprojects {
     dependencies {
         "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.10.2")
         "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    }
+
+    extensions.configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+            }
+        }
     }
 
     tasks.withType<Test>().configureEach {
