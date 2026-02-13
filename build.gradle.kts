5c2ea7a97fc8c0d816a6538f46eb91fc6f597057
kzheart <kzheartgyf@gmail.com>
2026-02-13 18:03:07 +0800

    chore(release): bump version to 1.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.publish.maven.MavenPublication
 
 allprojects {
     group = "com.pi4j"
-    version = "1.0.1-SNAPSHOT"
+    version = "1.0.2-SNAPSHOT"
 
     repositories {
         mavenCentral()
